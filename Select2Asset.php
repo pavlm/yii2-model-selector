4b1805ed8c2fc52b30529fae93ebdfd9c6aa07e0
pavlm <pavlm.dev@gmail.com>
2015-06-18 17:05:15 +0300

    locale js autoload

--- Select2Asset.php
+++ Select2Asset.php
@@ -1,10 +1,16 @@
 <?php
 namespace pavlm\modelSelector;
 
+use Yii;
 use yii\web\AssetBundle;
 
 class Select2Asset extends AssetBundle
 {
+    /**
+     * @var boolean|string - if 'true' then uses current language 
+     */
+    public $locale = true;
+    
     public $sourcePath = '@vendor/bower/select2';
     
     public $js = [
@@ -20,5 +26,13 @@ class Select2Asset extends AssetBundle
     ];
     
     public $publishOptions = [];
-    
+
+    public function init()
+    {
+        parent::init();
+        if ($this->locale !== false) {
+            $lang = $this->locale === true ? strtolower(substr(Yii::$app->language, 0, 2)) : $this->locale;
+            $this->js[] = 'select2_locale_' . $lang . '.js';
+        }
+    }
 }
