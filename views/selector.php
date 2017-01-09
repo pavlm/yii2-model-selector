e4138944101b8d819f6c93058f50856af684b4a3
pavlm <pavlm.dev@gmail.com>
2017-01-09 21:56:27 +0300

    only long php tags

--- views/selector.php
+++ views/selector.php
@@ -20,19 +20,19 @@ if ($widget->itemLink) {
 $wrapId = $widget->id . '-wrap';
 ?>
 
-<?
+<?php
 echo Html::beginTag('div', array_merge($defaultOptions, $widget->options, ['id' => $wrapId, 'data-options' => ($widget->manualInit ? $opts : null)]));
 ?>
-	<? 
+	<?php 
 	$name = $widget->name ?: Html::getInputName($widget->model, $widget->attribute);
 	echo Html::textInput($name, null, array_merge($widget->options, ['id' => $widget->id, 'class' => 'ms-field form-control'])); 
 	?>
-	<? if ($widget->itemLink): // !empty($opts['model']['link']) ?>
-    	<? echo Html::a('<i class="glyphicon glyphicon-share-alt"></i>', 
+	<?php if ($widget->itemLink): // !empty($opts['model']['link']) ?>
+    	<?php echo Html::a('<i class="glyphicon glyphicon-share-alt"></i>', 
     	    $selected ? $opts['model']['link'] : '#', 
     	    ['class' => 'btn btn-default input-group-addon ms-link', 'target' => '_blank', 'disabled' => !$selected]); ?>
-	<? endif; ?>
-<?
+	<?php endif; ?>
+<?php
 echo Html::endTag('div');
 if (!$widget->manualInit) {
     $this->registerJs("\$('#".$wrapId."').modelSelector(".Json::encode($opts).");", View::POS_READY, $wrapId);
